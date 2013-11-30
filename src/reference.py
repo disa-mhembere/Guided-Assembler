3f50ed1ca6d6dc6ca9838512b82e42b8ea135b96
Disa Mhembere <dmhembe1@jhu.edu>
2013-11-29 21:51:03 -0500

    Reference & Target completed -- untested

--- src/reference.py
+++ src/reference.py
@@ -4,19 +4,21 @@
 # Created on 2013-11-26.
 # Email: disa@jhu.edu, slee320@jhu.edu
 # Copyright (c) 2013. All rights reserved.
-import numpy
+import numpy as np
 
 class Reference(object):
-  def __init__(self, R):
+  def __init__(self, len_R):
     """
     Object to hold a reference string and accompanying metadata associated with
     keeping track of how many matches/mismatches occur at each position
+
+    @param len_R: Is the length of the reference string. Note the actual
+    string is obtainable via the BWT so no need to hold it here.
     """
     self.look_up = {"A":0, "C":1, "G":2, "T":3}
-    self.R = R.upper() # technically don't need this since bwt will hold this anyway
 
-    # keeps track of the letters that have landed at a particular index
-    self.match_count = [[0]*4]*(len(R)) # 4 is for 'ACGT' --STRICTLY in that order! # FIXME
+    # keeps track of the letters that have landed at a particular index.
+    self.match_count = np.zeros((len(R),4))# 4 is for 'ACGT' --STRICTLY in that order!
 
   def match(self, idx, char, cnt=1):
     """
@@ -26,21 +28,53 @@ class Reference(object):
     @param char: the char that matched
     @param cnt: the number of times we should record char matched. Default=1
     """
-    self.match_count[idx][self.look_up[char]] += cnt
+    self.match_count[idx, self.look_up[char]] += cnt
 
-  def build_hist(self, ):
+  def build_hist(self, coverage, show=False, save=False, save_fn="max_hist_plot"):
     """
-    Build a histogram to determine where the contigs we find are located
-    indicating where
-    """
-    raise NotImplementedError("Build histogram function not yet impl.")
+    Build a histogram to determine what the maxes look & visualize match_count
+    Might be used to determine a resonable threshold
 
-  def get_contigs(self, ):
-    """
-    Use thresholding to determine what is a contig via histogram built
-    """
-    raise NotImplementedError("Build histogram function not yet impl.")
+    @param coverage: the average coverage for an single nt
+    @param show: Show visualization with match maxes
+    @param save_fn: Save to disk with this file name or else it will be the default
 
+    @return: the histogram array
+    """
+    import matplotlib.pyplot as plt
+
+    maxes = self.match_count.max(1) # get maxes along 1st dim
+
+    h = plt.hist(maxes, bins=coverage) # figure out where the majority
+    if show: plt.show()
+    if save: plt.savefig(save_fn, dpi=160, frameon=False)
+
+    return h[0]
+
+  def get_contigs(self, thresh):
+    """
+    Use thresholding to determine what is a contig using the match_count
+
+    @param thresh: anything under this value will not be included in the
+    """
+    contigs = list()
+    curr_contig = ""
+
+    for idx in xrange(self.match_count.shape[0]):
+      if not curr_contig: curr_contig_st_idx = idx # start position of contig in R
+
+      mx_idx = self.match_count[idx].argmax()
+      if self.match_count[idx, mx_idx] > thresh:
+        curr_contig += "ACGT"[mx_idx] # Note assumption of ACGT here again
+      else:
+        if curr_contig: # If its not empty
+          contigs.append(curr_contig, curr_contig_st_idx)
+          curr_contig = ""
+
+    # Add last contig if it exists
+    if curr_contig: contigs.append(curr_contig, curr_contig_st_idx)
+
+    return contigs
 
 def test():
   ref = Reference("ACGTACT")
--- src/target_reads.py
+++ src/target_reads.py
@@ -6,16 +6,20 @@
 # Copyright (c) 2013. All rights reserved.
 
 import numpy as np
+import random
+import pdb
 
 class Target(object):
-  def __init__(self, p, read_length, T):
+  def __init__(self, p, read_length, T, seed=None, coverage=5):
     """
     An Object that enables the partitioning &generation of synthetically mutated
     data given a full Target sequence read
 
-    @param p: the probability of a polymorphism in any single letter with the read
+    @param p: the probability of having a polymorphism at any single letter within the read
     @param read_length: read lengths that we will produce
     @param T: the target string which we are trying to assemble using R, the ref
+    @param seed: seed for random
+    @param coverage: max redundancy at any single nt position
     """
     assert (p <= 1 and p >= 0), "Probability must be in range [0:1] inclusive"
     assert isinstance(T, str), "Target (T) must be a string"
@@ -24,14 +28,15 @@ class Target(object):
     self.T = T.upper() # All upper case
     self.p = p
     self.read_length = read_length
+    self.max_mutations = int(self.read_length*p) # important
+    self.coverage = coverage
 
-    self.seen = list() # list of seen start indexes
+    if seed: random.seed(seed) # for result reproducibility
+    self.seen = [0]*coverage # list of seen start indexes
 
   def get_read(self, **kwargs):
     """
-    Get a single read from the Target read
-
-    @todo: SL
+    Get a single read from the Target sequence for a streaming read model
 
     @keyword p: the probability of SNP occuring
     @keyword read_length: the read length
@@ -44,43 +49,62 @@ class Target(object):
     else: read_length = self.read_length
 
     assert set(kwargs.keys()).issubset(set(["p", "read_length"]))\
-                                            , "Unknown keyword argument in input"
+                                            ,"Unknown keyword argument in input"
 
     # pick idx in target string where to start from
     idx = np.random.random_integers(0, high=len(self.T)-read_length) # The range is inclusive
+    while(self.seen[idx] > self.coverage): # don't over cover
+      idx = np.random.random_integers(0, high=len(self.T)-read_length) # The range is inclusive
+
     read = self.T[idx:read_length]
 
-    read = self.mutate(read, p) # TODO: SL
+    read = self.mutate(read, p) # A SNPs at random indexes
 
     self._update_seen(idx)
     return read
 
-  def mutate(self, read, p):
+  def mutate(self, read):
     """
     Use p to determine how to add SNPs to the returned string
 
-    @todo: SL
-
     @param: p the probability of SNP occurring # TODO SL verify
-    @return: a string with some possible SNPs added
+    @return: a bytearray with some possible SNPs added
     """
-    raise NotImplementedError("***TODO: SL****")
 
+    # Get the number of mutations to apply
+    num_mutations = random.randint(0, self.max_mutations)
+    mut_idx = random.sample(range(len(read)), num_mutations) # indices to apply mutation
+    read = bytearray(read)
+
+    incoming = read
+
+    for idx in mut_idx:
+      read[idx] = random.choice("ACGT")
+
+    if not read == incoming: print "Diff! %s != %s", (incoming, read)
+    return read
 
   def _update_seen(self, idx):
     """
     Update the reference to seen start indicies so we dont continuously sample
     the same intervals. Private method.
 
+    @param idx: The start index of the target string from where we just extracted a read
     """
     assert isinstance(idx, int), "Index must be an integer"
-    self.seen.append(idx)
-
-    self.seen
+    self.seen[idx] += 1
 
   def get_read_list(self, **kwargs):
     """
-    Return a list of randomly sampled strings with possible SNPs given T
+    Return a list of even-coverage randomly sampled strings with possible SNPs
+    given T. For the non-streaming version of the read splitting.
+
+    Optional:
+    --------
+    @keyword p: the probability of SNP occuring
+    @keyword read_length: the read length
+    @keyword save: boolean save or don't to disk
+    @keyword save_fn: the filename you want to use to write to disk
     """
 
     if kwargs.has_key("p"): p = kwargs["p"]
@@ -89,24 +113,34 @@ class Target(object):
     if kwargs.has_key("read_length"): p = kwargs["read_length"]
     else: read_length = self.read_length
 
-    assert set(kwargs.keys()).issubset(set(["p", "read_length"]))\
+    assert set(kwargs.keys()).issubset(set(["p", "read_length", "save", "save_fn"]))\
                                             , "Unknown keyword argument in input"
 
-    raise NotImplementedError("TODO: SL ")
+    mutated_reads = list()
+
+    for cov_idx in xrange(self.coverage+1):
+      reads = [ self.T[l:l+read_length] for l in xrange(cov_idx, len(self.T)-read_length+1) ]
+      mutated_reads.extend(map(self.mutate, reads))
+
+    if kwargs.get("save", False):
+      import datetime as dt
+      save_fn = kwargs.get("save_fn", "mutated_reads%s"(str(dt.datetime.now())))
+
+      np.save(save_fn, mutated_reads)
+    return mutated_reads
 
 def test():
-  T = Target(0.1, 6, "acgttttacccgggttac")
-  T._update_seen(4)
-  print "Seen so far = ", T.seen
-  try:
-    print T.get_read()
-    print get_ris
-  except Exception, msg:
-    print msg
+
+  seq = "acgttttacccgggttac"
+  T = Target(p=0.1, read_length=6, T=seq, seed=123, coverage=3)
+
+  read_list = map(str,T.get_read_list())
+  print read_list
+
+  pdb.set_trace()
 
 def main():
   test()
-
 
 if __name__ == "__main__":
   main()
